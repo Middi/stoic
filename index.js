b66a32fca58a2338cfdb0c2adfdcc7b23b623b73
“Richard Middleton” <Richard@richardmiddleton.me>
2018-08-25 15:03:25 +0200

    working ejs example

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const stoicapi = require("stoic-api").random();
+const stoicapi = require("stoic-api");
 
 console.log(stoicapi);
+
+app.engine('.ejs', require('ejs').__express);
+app.set('views', __dirname + '/client');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index', { quote: stoicapi.random() });
+});
 
 app.listen(port, () => {
     console.log(`App is running on port ${port}`);
